d2e44f90cf272c8f7bb3269bd335f55ee163c644
DavidRialVega <david.r.3.a@gmail.com>
2018-01-31 17:25:04 +0100

    Edit product
    Includes the view, css and js of edit product

---
+++ classimax.v-1/js/edit_single_product.js
@@ -0,0 +1,36 @@
+function add_product_tag(){
+    var tag_text;
+    tag_text = $("#tf_tag").val();
+    $("#div_tags").append('<span class="badge badge-pill badge-primary">' + tag_text + '</span>');
+    $("#tf_tag").val("");
+}
+
+function add_table_row(){    
+    var specification_row = '<tr><td><div class="form_group"><input type="text" class="form-control w-bg" placeholder="Titulo"></div></td><td><div class="form_group"><input type="text" class="form-control w-bg" placeholder="Especificación"></div></td></tr>';
+    $('#specification_table').append(specification_row);
+}
+
+function delete_table_row(){
+    $('#specification_table tr:last-child').remove();
+}
+
+function open_file_browser(){
+    $('#file_input').click();
+}
+
+function preview_image(){
+    console.log("Entro en el metodo");
+    //var img_div = '<div class="col-md-2"><img class="w-100"></div>';
+    var file    = document.querySelector('#file_input').files[0];
+    var reader  = new FileReader();
+    var src;
+    
+    reader.onloadend = function () {
+        src = reader.result;
+        $('#images_row').prepend('<div class="col-md-2"><img class="w-100" src="'+ src +'"></div>');
+    }
+    
+    if (file) {
+        reader.readAsDataURL(file);
+    }   
+}
