7a2a943ae082fdac72c8ec2911ad080beda19c2d
DavidRialVega <david.r.3.a@gmail.com>
2018-02-12 16:44:56 +0100

    Commit test

--- Dawllapop/src/main/java/entities/Product.java
+++ Dawllapop/src/main/java/entities/Product.java
@@ -1,5 +1,14 @@
 package entities;
 
-public class Product {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	
 }
