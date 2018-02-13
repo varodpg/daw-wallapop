0335277ece78d4fcf26b4880716dc9889f38e80b
alberalgo <alberalgo@gmail.com>
2018-02-13 20:11:28 +0100

    Products with Mustache
    15 products by example with mustache on category view

--- Dawllapop/src/main/java/com/practicaDaw/Dawllapop/controller/CategoryController.java
+++ Dawllapop/src/main/java/com/practicaDaw/Dawllapop/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.practicaDaw.Dawllapop.controller;
 
 import com.practicaDaw.Dawllapop.Entities.Category;
+import com.practicaDaw.Dawllapop.Entities.Product;
 import com.practicaDaw.Dawllapop.Repository.CategoryRepository;
 import com.practicaDaw.Dawllapop.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,25 +10,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
+
 @Controller
-@RequestMapping("/category")
-
-
 public class CategoryController {
 
 		@Autowired
 		private CategoryRepository repository;
+		@Autowired
+		private ProductRepository repository2;
 
 		@PostConstruct
 		public void init() {
-
 			Category c= new Category("Informatica");
-			repository.save(c);
-
+			//repository.save(c);
 		}
 
 
-
+		@RequestMapping("/categorias")
+		public String Categorias(Model model) {
+			
+			model.addAttribute("productos", repository2.findAll());
+			
+			return "category";
+		}
 
 
 }
