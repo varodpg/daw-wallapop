44caf90225f140ed029d2c4c0ee77d3018be6c4e
Alvaro <varodpg@gmail.com>
2018-02-14 02:18:59 +0100

    Security classes re-upload

--- Dawllapop/src/main/java/com/practicaDaw/Dawllapop/App.java
+++ Dawllapop/src/main/java/com/practicaDaw/Dawllapop/App.java
@@ -2,6 +2,7 @@ package com.practicaDaw.Dawllapop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@ComponentScan({"com.practicaDaw.Dawllapop.Repository"})
 public class App {
 
 	public static void main(String[] args) {
