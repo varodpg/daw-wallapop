207d78642730aa61df7f3c5130b9ee7208eb5585
juanmaverano <juanmita10@hotmail.com>
2018-02-22 20:38:51 +0100

    edit single controller

--- Dawllapop/src/main/java/com/practicaDaw/Dawllapop/controller/SingleController.java
+++ Dawllapop/src/main/java/com/practicaDaw/Dawllapop/controller/SingleController.java
@@ -2,6 +2,7 @@ package com.practicaDaw.Dawllapop.controller;
 
 import com.practicaDaw.Dawllapop.Entities.Category;
 import com.practicaDaw.Dawllapop.Entities.Offer;
+import com.practicaDaw.Dawllapop.Entities.Product;
 import com.practicaDaw.Dawllapop.Repository.CategoryRepository;
 import com.practicaDaw.Dawllapop.Repository.OfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,27 +10,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
+
 @Controller
 
 public class SingleController {
 
+	@Autowired
+	private OfferRepository repository;
+
+	@PostConstruct
+	public void init() {
+
+		Offer o = new Offer(60, "Quiero este precio");
+		repository.save(o);
+
+	}
+
 	@RequestMapping("/single")
-	public String single() {
+	public String Categorias(Model model) {
+
 		return "single";
 	}
-		@Autowired
-		private OfferRepository repository;
-
-		@PostConstruct
-		public void init() {
-
-			Offer o= new Offer(60,"Quiero este precio");
-			repository.save(o);
-
-		}
-
-
-
 
 }
