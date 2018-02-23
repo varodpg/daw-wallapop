0b74208ffc59884ebdf012125d45371c8d867650
juanmaverano <juanmita10@hotmail.com>
2018-02-23 13:23:10 +0100

    Change edit-profile

--- Dawllapop/src/main/java/com/practicaDaw/Dawllapop/controller/UserProfileController.java
+++ Dawllapop/src/main/java/com/practicaDaw/Dawllapop/controller/UserProfileController.java
@@ -26,46 +26,52 @@ public class UserProfileController {
 	private UserRepository userRepository;
 	@Autowired
 	private UserComponent userComponent;
-	
-	
-	
-	
+
 	@RequestMapping("/add_new_user")
-	public String add_new_user(Model model, User user){		
+	public String add_new_user(Model model, User user) {
 		repository.save(user);
 		return "index";
 	}
-	
-	
-	@Autowired    
+
+	@RequestMapping("/edit_user")
+	public String edit_user(Model model, User user) {
+		repository.saveAndFlush(user);
+		return "index";
+	}
+	@RequestMapping("/delete_user")
+	public String delete_user(Model model, User user) {
+		repository.delete(user);
+		return "index";
+	}
+
+	@Autowired
 	private UserRepository repository;
 
 	@PostConstruct
 	public void init() {
 
-
-
-        User u = new User("Alvaro", "varo@hotmail.com","", "password",true, "Mostoles", "ROLE_ADMIN","ROLE_USER");
-        repository.save(u);
-		User u2 = new User("Juanma", "juanma@hotmail.com","", "password",true, "Alcorocn", "ROLE_USER");
+		User u = new User("Alvaro", "varo@hotmail.com", "", "password", true, "Mostoles", "ROLE_ADMIN", "ROLE_USER");
+		repository.save(u);
+		User u2 = new User("Juanma", "juanma@hotmail.com", "", "password", true, "Alcorocn", "ROLE_USER");
 		repository.save(u);
 		repository.save(u2);
 
 	}
+
 	@RequestMapping("/user-profile")
-	public String serveUserProfile(Model model, @RequestParam Optional<String> emailUser, @RequestParam Optional<String> passUser, @RequestParam Optional<Boolean> sent) {
-		if(sent.isPresent()) {
-			if(emailUser.isPresent()){
+	public String serveUserProfile(Model model, @RequestParam Optional<String> emailUser,
+			@RequestParam Optional<String> passUser, @RequestParam Optional<Boolean> sent) {
+		if (sent.isPresent()) {
+			if (emailUser.isPresent()) {
 				userComponent.getLoggedUser().setEmail(emailUser.get());
 			}
-			if(!passUser.get().equals("")) {
+			if (!passUser.get().equals("")) {
 				userComponent.getLoggedUser().setPasswordHash(passUser.get());
 			}
 			userRepository.save(userComponent.getLoggedUser());
 		}
-		
+
 		return "user-profile";
 	}
-
 
 }
