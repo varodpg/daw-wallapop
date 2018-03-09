1ec02a55c84ca84c78dcc8a3c2fbdede058b5366
DavidRialVega <david.r.3.a@gmail.com>
2018-03-09 20:08:39 +0100

    Updated deleteProdut from RestController

--- Dawllapop/src/main/java/com/practicaDaw/Dawllapop/ApiRestController/RestProduct.java
+++ Dawllapop/src/main/java/com/practicaDaw/Dawllapop/ApiRestController/RestProduct.java
@@ -1,23 +1,50 @@
 package com.practicaDaw.Dawllapop.ApiRestController;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practicaDaw.Dawllapop.Entities.Product;
+import com.practicaDaw.Dawllapop.Entities.User;
 import com.practicaDaw.Dawllapop.Repository.ProductRepository;
+import com.practicaDaw.Dawllapop.Repository.UserRepository;
+import com.practicaDaw.Dawllapop.services.ProductServices;
 
 @RestController
+@RequestMapping("/api/products")
 public class RestProduct {
 	@Autowired
-	private ProductRepository p_repository;
+	private ProductRepository pRepository;
+	@Autowired
+	private ProductServices pServices;
+	@Autowired
+	private UserRepository uRepository;
 	
-	@RequestMapping(value = "/api/deleteProduct/{id}", method = RequestMethod.DELETE)
-	public boolean deletedProduct(@PathVariable long id) {
-		Product p = p_repository.findOne(id);
-		p_repository.delete(p);
-		return true;
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Product> deletedProduct(@PathVariable long id) {
+		Product p = pRepository.findOne(id);
+		if(p != null) {
+			pRepository.delete(p);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}		
 	}
+	
+//	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+//	public ResponseEntity<List<Product>> getUserProducts(@PathVariable("id") long id){
+//		User user = uRepository.getOne(id);
+//		if(user != null) {
+//			List<Product> products = pServices.getAllProductsByUser(user.getId());
+//			return new ResponseEntity<>(products, HttpStatus.OK);
+//		}else {
+//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//		}					
+//	}
 }
