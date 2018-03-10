a1c4ec9f2bfd26afb9821e07d05ada03b9c03656
DavidRialVega <david.r.3.a@gmail.com>
2018-03-10 18:16:52 +0100

    Rest products
    Now you can use rest methods to get products

--- Dawllapop/src/main/java/com/practicaDaw/Dawllapop/ApiRestController/RestProduct.java
+++ Dawllapop/src/main/java/com/practicaDaw/Dawllapop/ApiRestController/RestProduct.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.practicaDaw.Dawllapop.Entities.Product;
 import com.practicaDaw.Dawllapop.Entities.User;
 import com.practicaDaw.Dawllapop.Repository.ProductRepository;
@@ -37,14 +38,26 @@ public class RestProduct {
 		}		
 	}
 	
-//	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
-//	public ResponseEntity<List<Product>> getUserProducts(@PathVariable("id") long id){
-//		User user = uRepository.getOne(id);
-//		if(user != null) {
-//			List<Product> products = pServices.getAllProductsByUser(user.getId());
-//			return new ResponseEntity<>(products, HttpStatus.OK);
-//		}else {
-//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//		}					
-//	}
+	@JsonView(Product.BasicInformation.class)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Product> getProduct(@PathVariable long id){
+		Product product = pRepository.getOne(id);
+		if(product != null) {
+			return new ResponseEntity<>(product, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@JsonView(Product.BasicInformation.class)
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+	public ResponseEntity<List<Product>> getUserProducts(@PathVariable("id") long id){
+		User user = uRepository.getOne(id);
+		if(user != null) {
+			List<Product> products = pServices.getAllProductsByUser(user.getId());
+			return new ResponseEntity<>(products, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}					
+	}	
 }
