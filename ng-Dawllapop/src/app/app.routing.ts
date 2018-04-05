0507969dac49afef02d87baa2927f3a141a46b14
juanmaverano <juanmita10@hotmail.com>
2018-04-05 17:38:49 +0200

    addProduct routing added

--- ng-Dawllapop/src/app/app.routing.ts
+++ ng-Dawllapop/src/app/app.routing.ts
@@ -2,11 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { SingleProductComponent } from './single-product/single-product.component';
 import { CategoryComponent } from './category/category.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 const appRoutes = [
     { path: 'index', component: IndexComponent},
     { path: 'singleProduct/:id', component: SingleProductComponent },
     { path: '', redirectTo: 'index', pathMatch: 'full' },
     { path: 'category', component: CategoryComponent },
+    { path: 'addProduct', component: AddProductComponent}
 ]
 export const routing = RouterModule.forRoot(appRoutes);
