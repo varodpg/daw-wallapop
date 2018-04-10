131736fcc83215630c893aa0e505b042fab48c9a
daniel <cens097541@gmail.com>
2018-04-10 21:29:59 +0200

    Revert "unfucked merge"
    This reverts commit 5065d990531ec3e476f21c6b2cf4bd7d29fab6a9.

--- ng-Dawllapop/src/app/service/product.service.ts
+++ ng-Dawllapop/src/app/service/product.service.ts
@@ -8,9 +8,13 @@ const LAST_PRODUCTS_URL = "https://localhost:8443/api/products/"
 const GET_SINGLE_PRODUCT_URL = "https://localhost:8443/api/products/"
 const ADD_PRODUCT_URL = "https://localhost:8443/api/products/"
 const GET_CATEGORY_PRODUCTS = "https://localhost:8443/api/products/category/"
+<<<<<<< HEAD
+const EDIT_PRODUCT = "https://localhost:8443/api/products/"
+=======
 const GET_CATEGORY_PRODUCTS_TOTAL = "https://localhost:8443/api/products/category/"
 const SEARCH_PRODUCTS_URL = "https://localhost:8443/api/products/search"
-const EDIT_PRODUCT = "https://localhost:8443/api/products"
+>>>>>>> c9a24a2bb5f9d9e814f2e03904a277098fdd68c5
+
 @Injectable()
 export class ProductService {
     constructor(private http: HttpClient) {}
@@ -41,9 +45,5 @@ export class ProductService {
 
     searchProducts(search: String){
         return this.http.get<Product[]>(SEARCH_PRODUCTS_URL + "/" + search);
-    }
-    
-    getCategoryNumberProducts(id: number | string){
-        return this.http.get<number>(GET_CATEGORY_PRODUCTS + id + "/total");		
     }
 }
