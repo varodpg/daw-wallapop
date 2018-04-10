5065d990531ec3e476f21c6b2cf4bd7d29fab6a9
daniel <cens097541@gmail.com>
2018-04-10 21:02:54 +0200

    unfucked merge

--- ng-Dawllapop/src/app/service/user.service.ts
+++ ng-Dawllapop/src/app/service/user.service.ts
@@ -3,11 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { User } from "../model/product.model";
 
 const GET_USER_URL = "https://localhost:8443/api/users"
-<<<<<<< HEAD
-=======
 const SEARCH_USERS_URL = "https://localhost:8443/api/users/search"
-
->>>>>>> c9a24a2bb5f9d9e814f2e03904a277098fdd68c5
 @Injectable()
 export class UserService {
     constructor(private http: HttpClient) {}
@@ -16,15 +12,12 @@ export class UserService {
         return this.http.get<User>(GET_USER_URL + "/" + id);			
     }
 
-<<<<<<< HEAD
     addUser(user: User){
         return this.http.post(GET_USER_URL,user);
     }
 
 
-=======
     searchUsers(search: string){
         return this.http.get<User[]>(SEARCH_USERS_URL + "/" + search);
     }
->>>>>>> c9a24a2bb5f9d9e814f2e03904a277098fdd68c5
 }
